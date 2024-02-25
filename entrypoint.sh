b9dc128b3bb03139d6aef28b48d72f367388e426
Jonathan <52703336+Gabo-Dev@users.noreply.github.com>
2024-02-25 20:30:41 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "================="
 
     git config --global user.name "${GITHUB_ACTOR}"
     git config --global user.email "${GITHUB_EMAIL}"
-    git config --global --add safe.dorectory /github/workspace
+    git config --global --add safe.directory /github/workspace
 
     python3 /usr/bin/feed.py
     git add -A && git commit -m "Update Feed"
